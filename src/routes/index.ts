fde9d5816a32dc36190086244ce16523a8dba5d5
ssisaias <luiz.isaias95@gmail.com>
2020-07-17 22:24:30 -0300

    User model route service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router, response } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 routes.get('/', (req, resp) => resp.json({ message: 'aaaa' }));
 
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+/* eslint-disable class-methods-use-this */
+import { getRepository } from 'typeorm';
+import User from '../models/Users';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    // Só precisa criar repositório se for utilizar método personalizado
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Email already in use');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
